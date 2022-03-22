7da1512a531d22c5787a6b0b8f78d560e0e5aada
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-22 22:55:58 +0200

    create utils folder and move test-utils there + rename 'test-setup' to 'setup'

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'vitest';
-import App from './App';
-import { render, screen } from './test/test-utils';
+import App from '../App';
+import { render, screen } from '../utils/test-utils';
 
 describe('Simple working test', () => {
   it('the title is visible', () => {
     render(<App />);
-    expect(screen.getByText(/Wordle/i)).toBeInTheDocument();
+    expect(screen.getByText(/Wordle RO/i)).toBeInTheDocument();
   });
 });
