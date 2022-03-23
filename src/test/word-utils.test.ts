917bdf2bf0da83780268642712341a557d05dc64
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-23 22:07:34 +0200

    create and test isWordValid utils function

--- src/test/word-utils.test.ts
+++ src/test/word-utils.test.ts
@@ -1,9 +1,19 @@
 import { describe, expect, it } from 'vitest';
 import { WORD_LENGTH } from '../constants/settings';
-import { getRandomWord } from '../utils/word-utils';
+import { getRandomWord, isWordValid } from '../utils/word-utils';
 
-describe('word-utils', () => {
+describe('getRandomWord', () => {
   it('get random word', () => {
     expect(getRandomWord().length).toEqual(WORD_LENGTH);
+  });
+});
+
+describe('isWordValid', () => {
+  it('word is valid', () => {
+    expect(isWordValid('ninge')).toEqual(true);
+  });
+
+  it('word is not valid', () => {
+    expect(isWordValid('lemon')).toEqual(false);
   });
 });
--- src/utils/word-utils.ts
+++ src/utils/word-utils.ts
@@ -4,3 +4,7 @@ export const getRandomWord = () => {
   const randomIndex = Math.floor(Math.random() * wordlist.length);
   return wordlist[randomIndex];
 };
+
+export const isWordValid = (word: string) => {
+  return wordlist.includes(word);
+};
