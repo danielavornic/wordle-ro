3f0b0bd7e67d4a4783fb745f598486e1c5e3c43d
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-30 23:15:04 +0300

    render correct styles of grid cells according to status

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react';
 
-import { ROWS_COUNT } from '../../constants/settings';
+import { getRandomWord } from '../../utils/word-utils';
 import GridRow from './GridRow';
 
 const Grid: FC = () => {
+  const answer = getRandomWord();
+
   return (
     <div>
-      {[...Array(ROWS_COUNT)].map((elem, idx) => (
-        <GridRow key={idx} word={'myth'} />
-      ))}
+      <GridRow word={'acasa'} answer={answer} />
+      <GridRow word={'ninge'} answer={answer} />
+      <GridRow word={'soare'} answer={answer} />
+      <GridRow word={'curat'} answer={answer} />
+      <GridRow word={'fiica'} answer={answer} />
+      <GridRow word={'miros'} answer={answer} />
     </div>
   );
 };
--- src/components/grid/GridRow.tsx
+++ src/components/grid/GridRow.tsx
@@ -1,24 +1,31 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { WORD_LENGTH } from '../../constants/settings';
+import { LetterStatus } from '../../types/LetterStatus';
+import { computeGuess } from '../../utils/word-utils';
+
 import GridCell from './GridCell';
 
 interface Props {
   word: string;
+  answer: string;
 }
 
-const GridRow: FC<Props> = ({ word }) => {
+const GridRow: FC<Props> = ({ word, answer }) => {
   const remainingLetters = WORD_LENGTH - word.length;
   const letters = word.split('').concat(Array(remainingLetters).fill(''));
+
+  const [guessStatuses, setGuessStatuses] = useState<LetterStatus[]>([]);
+
+  useEffect(() => {
+    setGuessStatuses(computeGuess(word, answer));
+    console.log(answer);
+  }, [answer]);
 
   return (
     <div className='flex justify-center'>
       {letters.map((letter, idx) => (
-        <GridCell
-          key={idx}
-          letter={letter}
-          status={letter ? 'filled' : 'empty'}
-        />
+        <GridCell key={idx} letter={letter} status={guessStatuses[idx]} />
       ))}
     </div>
   );
